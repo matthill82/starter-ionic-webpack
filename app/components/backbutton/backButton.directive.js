8798cade63c748c9b964d362f3f3f8532ac1576f
matthew hill <matthill82@live.com>
2016-01-22 17:50:33 +0000

    added custom backbutton directive

---
+++ app/components/backbutton/backButton.directive.js
@@ -0,0 +1,29 @@
+/**
+ * BackButton Directive
+ * @namespace Directives
+ *
+ * @usage : <backbutton ng-click="goBack()"></backbutton>
+ */
+
+'use strict';
+
+let moduleName = 'siteSnap.backButton';
+
+class BackButton {
+	constructor () {
+		this.restrict = 'E';
+		this.replace = false;
+		this.scope = false;
+		this.template = `<button class="button icon button-clear"></button>`
+	}
+
+	compile(element) {
+		let icon = ionic.Platform.isIOS() ? 'ion-ios-arrow-back' : 'ion-android-arrow-back';
+		angular.element(element[0]).addClass(icon);
+	}
+}
+
+angular.module(moduleName, [])
+	.directive('backButton', BackButton);
+
+export default moduleName;
