c37be795d9fbd4be5692e11ae30e7f63107c40db
matthew hill <matthill82@live.com>
2016-01-23 14:43:16 +0000

    backbutton directive class tidied

--- app/components/backbutton/backButton.directive.js
+++ app/components/backbutton/backButton.directive.js
@@ -5,25 +5,30 @@
  * @usage : <backbutton ng-click="goBack()"></backbutton>
  */
 
-'use strict';
+"use strict";
 
-let moduleName = 'siteSnap.backButton';
+let moduleName = "siteSnap.backButton";
 
 class BackButton {
 	constructor () {
-		this.restrict = 'E';
+		this.restrict = "E";
 		this.replace = false;
 		this.scope = false;
 		this.template = `<button class="button icon button-clear"></button>`
 	}
 
 	compile(element) {
-		let icon = ionic.Platform.isIOS() ? 'ion-ios-arrow-back' : 'ion-android-arrow-back';
+		let icon = ionic.Platform.isIOS() ? "ion-ios-arrow-back" : "ion-android-arrow-back";
 		angular.element(element[0]).addClass(icon);
 	}
 }
 
 angular.module(moduleName, [])
-	.directive('backButton', BackButton);
+	.directive("backButton", () => new BackButton())
+	.run(function ($log) {
+		$log.debug("siteSnap.backbutton directive - run");
+	});
+
+BackButton.$inject = ["$log"];
 
 export default moduleName;
